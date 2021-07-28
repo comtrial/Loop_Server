a944c1070364f3faaa8a584a88c78acd5803c287
jjo3ys <jjo3ys@gmail.com>
2021-07-28 12:32:22 +0900

    <feat>auth_user custom 모델

--- user_api/models.py
+++ user_api/models.py
@@ -3,11 +3,25 @@ from django.db import models
 # Create your models here.
 from django.conf import settings
 from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
+DEPARTMENT_CHOICES = (
+    (0, "산업경영공학과"),
+    (1, "기계공학과"),
+    (2, "컴퓨터공학과"),
+    (3, "그외")
+)
+
+class UserCustom(AbstractUser):#이미 makemigrations-migrate를 한경우AbstractUser가 적용이 안됨 auth_user를 다 설계한 이후에 migrations절차 진행 요망
+    department = models.IntegerField(choices=DEPARTMENT_CHOICES)
+    class Meta:
+        db_table = "auth_user"
+
+        
 # token 형성 logic
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created: 
-        Token.objects.create(user = instance)
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_auth_token(sender, instance=None, created=False, **kwargs):
+#     if created: 
+#         Token.objects.create(user = instance)
