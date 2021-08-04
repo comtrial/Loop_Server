bbd43dcde2d02a212895ae53d2df38d9a5c8ec1e
yeonsung Joe <jjo3ys>
2021-08-04 19:51:36 +0900

    <feat> comment, comment_image upload

--- feed_api/models.py
+++ feed_api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 # for store user data by authorization
 from django.contrib.auth.models import User
 from django.conf import settings
+
 class Feed(models.Model):
-    # on_delete=models.CASCADE : 작성자 탈퇴시 게시글 삭제
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     id = models.AutoField(db_column='id', primary_key=True)
     title = models.CharField(max_length=100)
@@ -14,10 +14,19 @@ class Feed(models.Model):
     def __str__(self):
         return self.title
 
-class Image(models.Model):
+class FeedImage(models.Model):
     feed = models.ForeignKey('Feed', related_name='feed_image', on_delete=models.CASCADE)
     image = models.ImageField(null=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    feed = models.ForeignKey('Feed', related_name='feed_comment', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     
-    # def __str__(self):
-    #     return self.image
+class CommentImage(models.Model):
+    comment = models.ForeignKey('Comment', related_name='comment_image', on_delete=models.CASCADE)
+    image = models.ImageField(null=True)
+    
 
--- feed_api/urls.py
+++ feed_api/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('upload', views.upload),
     # path('upload', views.FeedViewSet),
     path('home_load', views.home_load),
-    path('detail_load/<idx>/', views.detail_load)
+    path('detail_load/<idx>/', views.detail_load),
+    path('comment_upload/<idx>/', views.comment_upload)
 ]
