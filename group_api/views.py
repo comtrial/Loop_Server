a693f6cd00d0c1652923f0fdcef72813369b4516
Gilhan Yong <rlfgks97@gmail.com>
2021-09-07 01:35:19 +0900

    <fix> auto create self

--- group_api/views.py
+++ group_api/views.py
@@ -2,7 +2,7 @@ from json.decoder import JSONDecoder
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .serializers import GroupImageSerializer, GroupSerializer, CrewSerializer
-from .models import  Group, Crew, GroupImage
+from .models import Group, Crew, GroupImage
 from user_api.models import UserCustom
 import json
 
@@ -10,13 +10,15 @@ from rest_framework import status
 from rest_framework.status import HTTP_201_CREATED, HTTP_202_ACCEPTED, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated 
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
 
 # Create your views here.
+
+
 @api_view(['POST', ])
 @permission_classes((IsAuthenticated,))
 def create_group(request):
@@ -24,7 +26,7 @@ def create_group(request):
 
     # 그루비룸 중복 체크
     try:
-        duplicate = Group.objects.get(group_name = request.data['group_name'])
+        duplicate = Group.objects.get(group_name=request.data['group_name'])
         # Group 명 중복 처리
         if duplicate != None:
             return Response("이미 있는 이름입니다.", status=HTTP_409_CONFLICT)
@@ -32,11 +34,18 @@ def create_group(request):
             pass
 
     except:
-        #MARK - make_Group logic
-        group = Group(group_leader = leader, group_name = request.data['group_name'], group_description = request.data['group_description'])
+        # MARK - make_Group logic
+        group = Group(group_leader=leader,
+                      group_name=request.data['group_name'], group_description=request.data['group_description'])
         group.save()
 
         group_sz = GroupSerializer(group)
+
+        crew = UserCustom.objects.get(id=request.user.id)
+        group = Group.objects.get(id=group_sz.data['id'])
+
+        crew = Crew(crew=crew, group=group)
+        crew.save()
 
         return Response(group_sz.data, status=HTTP_201_CREATED)
 
@@ -47,41 +56,40 @@ def create_group(request):
         #     group_sz.save()
 
         #     return Response(group_sz.data)
-        
-        
+
         # else:
         #     return Response('유효하지 않은 형식입니다.', status = status.HTTP_403_FORBIDDEN)
 
 # Create your views here.
+
+
 @api_view(['POST', ])
 @permission_classes((IsAuthenticated,))
 def create_crew(request, group_idx):
     user = request.user
 
-
     try:
-        crew = UserCustom.objects.get(id = request.data['crew'])
-        group = Group.objects.get(id = group_idx)
+        crew = UserCustom.objects.get(id=request.data['crew'])
+        group = Group.objects.get(id=group_idx)
 
         # crew 중복되는거 처리해줘야 될 거 같은데
 
-        crew= Crew(crew = crew, group = group)
+        crew = Crew(crew=crew, group=group)
         crew.save()
 
         crew_sz = CrewSerializer(crew)
-    
+
         return Response(crew_sz.data, status=status.HTTP_201_CREATED)
     except:
-        return Response('유효하지 않은 형식입니다.', status = status.HTTP_403_FORBIDDEN)
+        return Response('유효하지 않은 형식입니다.', status=status.HTTP_403_FORBIDDEN)
 
     # # Group 명 중복 처리
     # if crew_sz.is_valid():
     #     crew_sz.save()
     #     return Response(crew_sz.data, status=HTTP_201_CREATED)
-    
-    # else:
-    #     return Response('유효하지 않은 형식입니다.', status = status.HTTP_403_FORBIDDEN)  
 
+    # else:
+    #     return Response('유효하지 않은 형식입니다.', status = status.HTTP_403_FORBIDDEN)
 
 
 # Create your views here.
@@ -89,8 +97,8 @@ def create_crew(request, group_idx):
 @permission_classes((IsAuthenticated,))
 def read_group(request, group_idx):
 
-    group = Group.objects.get(id = group_idx)
-    
+    group = Group.objects.get(id=group_idx)
+
     group_sz = GroupSerializer(group)
     res_dict = group_sz.data
 
@@ -98,19 +106,17 @@ def read_group(request, group_idx):
     if group.group_leader.id == request.user.id:
         # 출력을 안하면 오류발생
         print("group_sz.data", group_sz.data)
-        res_dict = json.JSONDecoder().decode(json.JSONEncoder().encode(group_sz.__dict__['_data']))
+        res_dict = json.JSONDecoder().decode(
+            json.JSONEncoder().encode(group_sz.__dict__['_data']))
         res_dict['is_author'] = '1'
 
     return Response(res_dict, status=HTTP_201_CREATED)
-    
-    
 
 
 # Create your views here.
 @api_view(['GET', ])
 @permission_classes((IsAuthenticated,))
 def read_all_groups(request):
-    
 
     try:
         groups = Group.objects.all()
@@ -120,18 +126,14 @@ def read_all_groups(request):
     if request.method == "GET":
         group_sz = GroupSerializer(groups, many=True)
         print(group_sz.data)
-        
-
-
 
     return Response(group_sz.data, status=HTTP_201_CREATED)
-    
-    
+
 
 @api_view(['POST', ])
 @permission_classes((IsAuthenticated,))
 def group_profile_update(request, idx):
-    group = Group.objects.get(id = idx)
+    group = Group.objects.get(id=idx)
     if group.group_leader.id == request.user.id:
         try:
             print('드루와~~')
@@ -147,7 +149,7 @@ def group_profile_update(request, idx):
             print('드루와~~4')
             # group_img.profile_image = request.data['image']
             print('드루와~~5')
-            
+
             group.save()
             print('드루와~~')
             return_dict = {
